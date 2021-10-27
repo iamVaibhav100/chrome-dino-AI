c10a301f42f5fbc9a3c7384ddae2b1e9fba550b5
Vaibhav Shukla <82935120+iamVaibhav100@users.noreply.github.com>
2021-10-27 11:54:34 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+# Chrome Dinosaur AI
+
+ Teaching AI to play a recreation of the Google Chrome dinosaur game. The game was made using pygame, and the AI part is done with NEAT-Python.
+
+# Using the application
+
+ * Clone GitHub repository
+ * Download required dependencies: `$ pip install -r requirements.txt`
+ * `$ python main.py`
+ 
+# How the Neural Network is setup:
+
+ The neural network is configured with two inputs, one output, and 0 hidden layers.
+ 
+ * Inputs: Distance from the dinosaur to the nearest cactus, the dinosaurs y position.
+ * Outputs: Jump
+ 
+ I have it setup to start with 15 dinosaurs per generation, however this can be changed by going into the config.txt and changing `pop_size`. Most of the values in config.txt are default values. You can find a description of all of the settings [here](https://neat-python.readthedocs.io/en/latest/config_file.html)
+
+# Contribution
+
+  Any form of contribution is welcome! Please feel free to fork this project, modify/add to it, and make a pull request!
+
---
+++ main.py
@@ -0,0 +1,222 @@
+import pygame
+import neat
+import sys
+import os
+import math
+import random
+from pygame.locals import *
+
+pygame.init()
+clock = pygame.time.Clock()
+
+# Options
+WINDOW_SIZE = (1000, 500) # (Width, Height)
+DRAW_LINES = False # (Draw lines between the dinosaur and cactus to see what the AI sees)
+
+screen = pygame.display.set_mode(WINDOW_SIZE)
+display = pygame.Surface(WINDOW_SIZE)
+
+GROUND_LEVEL = WINDOW_SIZE[1]/2 + 75
+ground_rect = pygame.Rect(0, GROUND_LEVEL, WINDOW_SIZE[0], WINDOW_SIZE[1])
+
+dinosaur_img = pygame.image.load('data/dinosaur.png').convert_alpha()
+cactus_img = pygame.image.load('data/cactus.png').convert_alpha()
+font = pygame.font.Font('data/roboto.ttf', 25)
+
+generation = 0
+
+class Dinosaur():
+    def __init__(self, x, y, width, height, img): #img must be a pygame surface object
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.img = pygame.transform.scale(img, (width, height))
+        self.rect = pygame.Rect(x, y, width, height)
+        self.vertical_momentum = 0
+        self.onGround = False
+        self.last_closest_pipe = cacti[0] # Setting the closest cactus to the leftmost cactus by default
+
+    def update(self):
+        self.x, self.y = self.rect.x, self.rect.y # Updating position atributes
+        self.movement()
+
+    def draw(self):
+        display.blit(self.img, (self.x, self.y))
+
+    def jump(self):
+        if self.onGround:
+            self.vertical_momentum = -11
+
+    def movement(self):
+        self.rect.y += self.vertical_momentum
+
+        if self.rect.colliderect(ground_rect):
+            self.onGround = True
+        else:
+            self.onGround = False
+
+        if self.onGround:
+            self.rect.bottom = ground_rect.top + 1 # Adding 1 so that the dinosaur continues to collide with the rect, instead of shaking up and down
+            # Prevent from falling through the ground
+            self.vertical_momentum = 0
+        else:
+            # Add gravity
+            self.vertical_momentum += 0.5
+
+        # Cap gravity
+        if self.vertical_momentum >= 40:
+            self.vertical_momentum = 40
+
+class Cactus():
+    def __init__(self, x, y, width, height, img, scroll_speed = 7):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.scroll_speed = scroll_speed
+        self.img = pygame.transform.scale(img, (width, height))
+        self.rect = pygame.Rect(x, y, width, height)
+
+    def update(self):
+        self.x -= self.scroll_speed # Moves cactus to the left
+        self.rect.x, self.rect.y = self.x, self.y # Update position atributes
+
+    def draw(self):
+        display.blit(self.img, (self.x, self.y))
+
+def get_distance(first_pos, second_pos):
+    # Distance formula
+    dx = first_pos[0] - second_pos[0]
+    dy = first_pos[1] - second_pos[1]
+    return math.sqrt(dx**2 + dy**2)
+
+def remove_dinosaur(index):
+    # 'Kills' the dinosaur and its corresponding genome and nn
+    dinosaurs.pop(index)
+    ge.pop(index)
+    nets.pop(index)
+
+def draw():
+    display.fill('white')
+
+    pygame.draw.line(display, (75, 75, 75), (0, GROUND_LEVEL), (WINDOW_SIZE[0], GROUND_LEVEL), 3)
+
+    for dinosaur in dinosaurs:
+        dinosaur.draw()
+        if DRAW_LINES:
+            pygame.draw.line(
+                display, 
+                (50, 200, 75), 
+                (dinosaur.rect.right, dinosaur.rect.centery), 
+                dinosaur.closest_pipe.rect.midtop,
+                2
+            )
+    for cactus in cacti:
+        cactus.draw()
+
+    alive_text = font.render(f'Number alive: {len(dinosaurs)}', 1, 'black')
+    generation_text = font.render(f'Generation: {generation}', 1, 'black')
+    display.blit(alive_text, (5, WINDOW_SIZE[1] - 40))
+    display.blit(generation_text, (5, WINDOW_SIZE[1] - 75))
+
+    screen.blit(display, (0, 0))
+
+    pygame.display.update()
+
+def main(genomes, config):
+    global cacti, dinosaurs, nets, ge, generation
+
+    cacti = [Cactus(WINDOW_SIZE[0] + 100, GROUND_LEVEL - 86, 50, 86, cactus_img)]
+
+    dinosaurs = []
+    nets = []
+    ge = []
+
+    scroll_speed = 7
+    generation += 1
+
+    for _, g in genomes:
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        nets.append(net)
+        dinosaurs.append(Dinosaur(100, GROUND_LEVEL-90, 80, 85, dinosaur_img))
+        g.fitness = 0
+        ge.append(g)
+
+    run = True
+    while run:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+        # Break if all the dinosaurs die
+        if len(dinosaurs) <= 0:
+            break
+
+        # Adding new cactus
+        if len(cacti) <= 1:
+            if cacti[0].x < random.randint(300, WINDOW_SIZE[0] - 200) + scroll_speed:
+                cacti.append(Cactus(WINDOW_SIZE[0] + 100, GROUND_LEVEL - 86, 50, 86, cactus_img, scroll_speed))
+
+        for cactus in cacti:
+            cactus.update()
+            if cactus.x < -100:
+                cacti.remove(cactus)
+            for i, dinosaur in enumerate(dinosaurs):
+                if dinosaur.rect.colliderect(cactus.rect):
+                    ge[i].fitness -= 3
+                    remove_dinosaur(i)
+
+        for i, dinosaur in enumerate(dinosaurs):
+            dinosaur.update()
+            # Check if the dinosaur passed a cactus
+            # Getting the closest cactus by finding the leftmost cactus that is to the right of the dinosaur
+            dinosaur.closest_pipe = [cactus for cactus in cacti if cactus.rect.x > dinosaur.x - dinosaur.width][0]
+            # Checking if the dinosaur passed a cactus by comparing it to the closest cactus in the last frame and seeing if there is a change
+            if dinosaur.closest_pipe != dinosaur.last_closest_pipe:
+                ge[i].fitness += 1
+                for cactus in cacti:
+                    # Increace speed everytime a cactus is passed
+                    cactus.scroll_speed += 0.05
+                    scroll_speed += 0.05 
+            dinosaur.last_closest_pipe = dinosaur.closest_pipe
+
+            # Giving all dinsoaurs a little fitness for staying alive
+            ge[i].fitness += 0.05
+
+            output = nets[i].activate(
+                (
+                    dinosaur.y,
+                    get_distance((dinosaur.x, dinosaur.y), dinosaur.closest_pipe.rect.midtop)
+                )
+            )
+
+            if output[0] > 0.5:
+                dinosaur.jump()
+
+        draw()
+
+# Setup the NEAT nn
+def run(config_path):
+    config = neat.config.Config(
+        neat.DefaultGenome,
+        neat.DefaultReproduction,
+        neat.DefaultSpeciesSet,
+        neat.DefaultStagnation,
+        config_path
+    )
+
+    p = neat.Population(config)
+
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    winner = p.run(main, 50)
+
+if __name__ == '__main__':
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config.txt')
+    run(config_path)
